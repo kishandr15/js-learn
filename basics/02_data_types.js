0c8815c2ff45eac2ec8a778ba53c81ef563882d8
kishandr15 <kishuhema@gmail.com>
2024-05-05 00:24:00 +0530

    updated data_types.js

--- basics/02_data_types.js
+++ basics/02_data_types.js
@@ -2,15 +2,34 @@
 
 // alert( 3 + 3);   // we are using node.js, so alert() is available only in browser.
 
-// Data Types
+// Data Types 
+// 1. Primitive Types
 
 let name = "kishan";    // string
 let age = 22;           // number
 let isApproved = true;  // boolean
-let firstName;          // undefined
-let temperature = null; // null (typeof null is object)
-                        // object
-
-console.log(typeof(temperature)); 
+let firstName;          // undefined (typeof undefined is undefined)
+let temperature = null; // null      (typeof null is object)
+const id = Symbol('123')    // Symbol
+const num = 99999999999n    // BigInt
 
 //typeof is an operator, which returns the type of the variable.
+console.log(typeof(temperature)); 
+
+
+
+// 2. Reference Types
+
+// Array
+let names = ['kishan', 'kumar', 'soni'];
+
+// Object
+let person = {
+    name: 'kishan',
+    age: 22
+};
+
+// Functions
+function greet() {
+    console.log('Hello World');
+}
